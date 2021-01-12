9e15dcaf486eb126e3445077ca37bf150e3b593c
sdvcrx <memory.silentvoyage@gmail.com>
2021-01-12 22:14:06 +0800

    tests: add gjson to benchmark

--- bench_test.go
+++ bench_test.go
@@ -3,37 +3,70 @@ package rejson
 import (
 	"encoding/json"
 	"testing"
+
+	"github.com/tidwall/gjson"
 )
 
+const (
+	benchmarkJSON = `{"widget":{"debug":"on","window":{"title":"Sample Konfabulator Widget","name":"main_window","width":500,"height":500},"image":{"src":"Images/Sun.png","hOffset":250,"vOffset":250,"alignment":"center"},"text":{"data":"Click Here","size":36,"style":"bold","vOffset":100,"alignment":"center","onMouseUp":"sun1.opacity = (sun1.opacity / 100) * 90;"}}}`
+)
+
+type benchWidget struct {
+	Name      string `jsonp:"widget.window.name"`
+	HOffset   int    `jsonp:"widget.image.hOffset"`
+	OnMouseUp string `jsonp:"widget.text.onMouseUp"`
+}
+
+type benchStruct struct {
+	Widget struct {
+		Window struct {
+			Name string `json:"name"`
+		} `json:"window"`
+		Image struct {
+			HOffset int `json:"hOffset"`
+		} `json:"image"`
+		Text struct {
+			OnMouseUp string `json:"onMouseUp"`
+		} `json:"text"`
+	} `json:"widget"`
+}
+
 func BenchmarkUnmarshalReJSON(b *testing.B) {
-	type user struct {
-		FirstName string `jsonp:"first_name"`
-		LastName  string `jsonp:"last_name"`
-		Age       int    `jsonp:"age"`
-		Empty     string `jsonp:"-"`
-	}
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		u := &user{}
-		Unmarshal(testUserJSON, u)
+		w := benchWidget{}
+		Unmarshal(benchmarkJSON, &w)
+	}
+}
+
+func BenchmarkUnmarshalGJSONGet(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		w := benchWidget{}
+		r := gjson.Parse(benchmarkJSON)
+		w.Name = r.Get("widget.window.name").String()
+		w.HOffset = int(r.Get("widget.image.hOffset").Int())
+		w.OnMouseUp = r.Get("widget.text.onMouseUp").String()
 	}
 }
 
 func BenchmarkUnmarshalEncodingJSON(b *testing.B) {
-	type user struct {
-		FirstName string `json:"first_name"`
-		LastName  string `json:"last_name"`
-		Age       int    `json:"age"`
-		Empty     string `json:"-"`
-	}
-	data := []byte(testUserJSON)
+	data := []byte(benchmarkJSON)
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		u := &user{}
-		json.Unmarshal(data, u)
+		bs := benchStruct{}
+		json.Unmarshal(data, &bs)
+
+		w := benchWidget{}
+		w.Name = bs.Widget.Window.Name
+		w.HOffset = bs.Widget.Image.HOffset
+		w.OnMouseUp = bs.Widget.Text.OnMouseUp
+
 	}
 }
